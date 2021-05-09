8271a33089ae4babdd87813bf3636eabb1b868cf
unknown <hamonangansitorus80@gmail.com>
2021-05-09 23:25:14 +0700

    Coba

---
+++ app/src/main/java/com/brillante/kotlite/ui/main/AccountActivity.kt
@@ -0,0 +1,12 @@
+package com.brillante.kotlite.ui.main
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.brillante.kotlite.R
+
+class AccountActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_account)
+    }
+}
