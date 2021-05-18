e4828a14be50d68c20870e9e136aa2733ac72c6d
unknown <hamonangansitorus80@gmail.com>
2021-05-18 21:46:28 +0700

    Login

---
+++ app/src/main/java/com/brillante/kotlite/model/LoginRequest.kt
@@ -0,0 +1,11 @@
+package com.brillante.kotlite.model
+
+import com.google.gson.annotations.SerializedName
+
+data class LoginRequest (
+    @SerializedName("username")
+    var username: String,
+
+    @SerializedName("password")
+    var password: String
+)
---
+++ app/src/main/java/com/brillante/kotlite/preferences/SessionManager.kt
@@ -0,0 +1,29 @@
+package com.brillante.kotlite.preferences
+
+import android.content.Context
+import android.content.SharedPreferences
+import com.brillante.kotlite.R
+
+class SessionManager (context: Context) {
+    private var prefs: SharedPreferences = context.getSharedPreferences(context.getString(R.string.app_name), Context.MODE_PRIVATE)
+
+    companion object {
+        const val USER_TOKEN = "user_token"
+    }
+
+    /**
+     * Function to save auth token
+     */
+    fun saveAuthToken(token: String) {
+        val editor = prefs.edit()
+        editor.putString(USER_TOKEN, token)
+        editor.apply()
+    }
+
+    /**
+     * Function to fetch auth token
+     */
+    fun fetchAuthToken(): String? {
+        return prefs.getString(USER_TOKEN, null)
+    }
+}
